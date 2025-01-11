7e269ac0abdc1d1d09487bee9811ae4f7f97c097
Anthea Ip <antheaip964@gmail.com>
2025-01-11 10:26:51 -0500

    Scraper; part 1 Extract

--- Scraper/my_first_scraper.py
+++ Scraper/my_first_scraper.py
@@ -10,16 +10,31 @@ def request_github_trending(url):
         raise Exception(f"Failed to fetch URL: {url}, Status code: {response.status_code}")
     return response.text
 
-# Part
+# Part 1: Extract
+def extract (page):
+    """Extracts the repo rows from the HTML content."""
+    soup = BeautifulSoup(page, "html.parser")
+    repo_rows = soup.find_all('article', class_='Box-row') #GitHub trending repositories are in 'article' tags with 'Box-row' class
+    return repo_rows
 
 
 
 if __name__ == "__main__":
     url = "https://github.com/trending"
     try:
+        # Part 0, Fetch page content
         page_content = request_github_trending(url)
         print("Request success")
-        print(page_content[:1000])
+        # print(page_content[:1000])
+
+        # Part 1, Extract
+        repo_rows = extract(page_content)
+        print(f"Number of repos extracted: {len(repo_rows)}")
+        # Print 1st repo's HTML to validate
+        if repo_rows:
+            print("First repo HTML:")
+            print(repo_rows[0].prettify()) # Prettifies HTML for easier reading
+
     except Exception as e:
         print(f"Error: {e}")
 
