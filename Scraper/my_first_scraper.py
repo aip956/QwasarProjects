173253bfa91bead85ea2175de71958be598213ef
Anthea Ip <antheaip964@gmail.com>
2025-01-11 10:16:12 -0500

    Scraper

--- Scraper/my_first_scraper.py
+++ Scraper/my_first_scraper.py
@@ -9,3 +9,14 @@ def request_github_trending(url):
     if response.status_code != 200:
         raise Exception(f"Failed to fetch URL: {url}, Status code: {response.status_code}")
     return response.text
+
+if __name__ == "__main__":
+    url = "https://github.com/trending"
+    try:
+        page_content = request_github_trending(url)
+        print("Request success")
+        print(page_content)
+    except Exception as e:
+        print(f"Error: {e}")
+
+        
