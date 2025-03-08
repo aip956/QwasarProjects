b2b4b0187f3bafe2ff7c914194f2f13fe4e57154
Anthea Ip <antheaip964@gmail.com>
2025-03-08 14:40:27 -0500

    method

---
+++ collab/methodologies.py
@@ -0,0 +1,60 @@
+
+def find_min(arr, start):
+    min = start;
+    print(arr)
+    for i in range(start + 1, len(arr)): # start+1 - len-1
+        if arr[i] < arr[min]:
+            min = i;
+    return min
+
+
+def selection(arr):
+    ind = 0
+    # len = arr.length - 1
+    
+    for ind in range (len(arr) - 1):
+        #subarr = arr [ind + 1, len
+        min = find_min(arr, ind)
+        arr[ind], arr[min] = arr[min], arr[ind] #swap elem with min of rest
+    return arr
+    
+arr = [0, 25, 73, 22, 11]
+sorted = selection(arr)
+print(sorted)
+
+
+
+
+
+
+
+# """
+# Selection Sort
+# Constraints:
+# length of array max(Int); [-9223372036854775808 to 9223372036854775807]
+# O(n), or sort in place
+
+# Pseudocode
+# [64, 25, 73, 22, 11]
+# 64, 11 -> [11, 25, 73, 22, 64]
+# 25, minRest = 22, -> [11, 22, 73, 25, 64]
+# 73, minRest = 25, -> [11, 22, 25, 73, 64]
+# 73, minRest = 64, -> [11, 22, 25, 64, 73]
+
+# [0, 25, 73, 22, 11]; ind = 0; min = 0
+# [0, 25, 73, 22, 11]; ind = 1; min = 11 -> [0, 11, 73, 22, 25]
+# [0, 11, 73, 22, 25]; ind = 2; min = 22 -> [0, 11, 22, 73, 25]
+# [0, 11, 22, 73, 25]; ind = 3; min = 25 -> [0, 11, 22, 25, 73]
+
+# iterate each ind
+# subarr from ind - end; find min; swap min and arr[ind]
+# ind++
+
+
+
+
+# Test by hand
+
+
+
+# """
