79a2692fbe363e8ff84ea5cef187160c60147024
Anthea Ip <antheaip964@gmail.com>
2025-05-26 16:45:08 -0400

    vector-store

--- ArcaneApp/vector_store.py
+++ ArcaneApp/vector_store.py
@@ -1,3 +1,29 @@
 import faiss
 import numpy as np
 import pickle
+
+class VectorStore:
+    def __init__(self, dim):
+        self.index = faiss.IndexFlatL2(dim)
+        self.texts = []
+
+
+    def add(self, embeddings, texts):
+        seflindex.add(nparray(embeddings).astype("float32"))
+        self.texts.extend(texts)
+        
+    def search(self, query_embedding, k=5):
+        D, I = self.index.search(np.array([query_embedding]).astype("float32"), k)
+        return [self.texts[i] for i in I[0]]
+    
+    def save(self):
+        faiss.write_index(self.index, "fiass.index")
+        with open("texts.pkl", "wb") as f:
+            pickle.dump(self.texts, f)
+
+    def load(self):
+        self.index = faiss.read_index("faiss.index")
+        with open("texts.pkl", "rb") as f:
+            self.texts = pickle.load(f)
+
+
